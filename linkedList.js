f9e6c297c7cd0b7e4f3b4ec749d5a58b39ecb05c
smmmfrd <smmmfrd@gmail.com>
2023-04-25 15:36:57 -0700

    Append Method and Recursive Print

--- linkedList.js
+++ linkedList.js
@@ -5,20 +5,40 @@ class Node {
 	}
 
 	print() {
+		return `${this.value}${this.next ? ", " + this.next.print() : "."}`
+	}
+
+	printValue() {
 		return `${this.value}`;
+	}
+
+	setNext(next) {
+		this.next = next;
 	}
 }
 
 class LinkedList {
 	constructor(value) {
 		this.head = new Node(value);
+		this.tail = this.head;
 	}
 
-	print() {
+	printList() {
 		return this.head.print();
+	}
+
+	printHeadTail() {
+		return `${this.head.printValue()}, ..., ${this.tail.printValue()}`;
+	}
+
+	append(value) {
+		const node = new Node(value);
+		this.tail.setNext(node);
+		this.tail = node;
 	}
 }
 
 const list = new LinkedList(1);
+list.append(2);
 
-console.log(list.print());
+console.log(list.printList());
