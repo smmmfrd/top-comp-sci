cafc2165c342253306cd93153023d5a5194558d3
smmmfrd <smmmfrd@gmail.com>
2023-04-27 11:31:58 -0700

    Binary Search Tree - Height-based Balanced Eval

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -40,14 +40,14 @@ class Tree {
     const cleanArr = [...new Set(arr)];
 
     // Sort arr
-    const sortedArr = cleanArr.sort((a,b) => a - b);
+    const sortedArr = cleanArr.sort((a, b) => a - b);
 
     // Build tree
     this.root = this.createTree(sortedArr, 0, sortedArr.length - 1);
   }
 
   createTree(array, start, end) {
-    if(start > end) return null;
+    if (start > end) return null;
 
     let mid = Math.floor((start + end) / 2);
 
@@ -59,12 +59,12 @@ class Tree {
   }
 
   insert(value, root = this.root) {
-    if(root === null) {
+    if (root === null) {
       root = new Node(value);
       return root;
     }
 
-    if(value < root.value) {
+    if (value < root.value) {
       root.left = this.insert(value, root.left);
     } else {
       root.right = this.insert(value, root.right);
@@ -74,22 +74,22 @@ class Tree {
   }
 
   delete(value, root = this.root) {
-    if(root === null) return root;
+    if (root === null) return root;
 
-    if(value < root.value) {
+    if (value < root.value) {
       root.left = this.delete(value, root.left);
       return root;
-    } else if (value > root.value){
+    } else if (value > root.value) {
       root.right = this.delete(value, root.right);
       return root;
     }
 
     // We only get here if this is the value to be deleted.
-    
-    if(root.left === null) {
+
+    if (root.left === null) {
       var temp = root.right;
       return temp;
-    } else if(root.right === null) {
+    } else if (root.right === null) {
       var temp = root.left;
       return temp;
     } else {
@@ -98,13 +98,13 @@ class Tree {
 
       // Find successor
       var succ = root.right;
-      
-      while(succ.left !== null) {
+
+      while (succ.left !== null) {
         parent = succ;
         succ = succ.left;
       }
 
-      if(parent.value != root.value) {
+      if (parent.value != root.value) {
         parent.left = succ.right;
       } else {
         parent.right = succ.right;
@@ -117,11 +117,11 @@ class Tree {
   }
 
   find(value, root = this.root) {
-    if(root.value === value) {
+    if (root.value === value) {
       return root;
     }
 
-    if(value < root.value) {
+    if (value < root.value) {
       return this.find(value, root.left);
     } else {
       return this.find(value, root.right);
@@ -129,17 +129,17 @@ class Tree {
   }
 
   levelOrder() {
-    if(this.root === null) return [];
+    if (this.root === null) return [];
 
     const queue = [this.root];
     const res = [];
     while (queue.length > 0) {
       let n = queue.shift();
       res.push(n.value);
-      if(n.left) {
+      if (n.left) {
         queue.push(n.left);
       }
-      if(n.right) {
+      if (n.right) {
         queue.push(n.right);
       }
     }
@@ -148,7 +148,7 @@ class Tree {
   }
 
   preorder(root = this.root, res = []) {
-    if(root === null) return [];
+    if (root === null) return [];
 
     res.push(root.value);
     res = [...res, ...this.preorder(root.left)];
@@ -158,7 +158,7 @@ class Tree {
   }
 
   inorder(root = this.root, res = []) {
-    if(root === null) return [];
+    if (root === null) return [];
 
     res = [...res, ...this.preorder(root.left)];
     res.push(root.value);
@@ -168,7 +168,7 @@ class Tree {
   }
 
   postorder(root = this.root, res = []) {
-    if(root === null) return [];
+    if (root === null) return [];
 
     res = [...res, ...this.preorder(root.left)];
     res = [...res, ...this.preorder(root.right)];
@@ -178,43 +178,51 @@ class Tree {
   }
 
   height(node, h = 0) {
-    if(node.left === null && node.right === null) {
+    if (node.left === null && node.right === null) {
       // If it has no children, it's a leaf node.
       return h;
     }
 
-    if(node.left === null) {
+    if (node.left === null) {
       return this.height(node.right, h + 1);
     }
-    if(node.right === null) {
+    if (node.right === null) {
       return this.height(node.left, h + 1);
     }
 
     // If it two children, return the lesser of their two heights.
     const left = this.height(node.left, h + 1);
     const right = this.height(node.right, h + 1);
-    return left > right ? right : left;
+    return left > right ? left : right;
   }
 
   depth(node, root = this.root, d = 0) {
-    if(node.value === root.value) return d;
+    if (node.value === root.value) return d;
 
-    if(node.value < root.value) {
+    if (node.value < root.value) {
       return this.depth(node, root.left, d + 1);
     } else {
       return this.depth(node, root.right, d + 1);
     }
+  }
+
+  isBalanced(root = this.root) {
+    const leftHeight = this.height(root.left);
+    const rightHeight = this.height(root.right);
+    return Math.abs(leftHeight - rightHeight) <= 1;
   }
 }
 
 const tree = new Tree();
 
 tree.buildTree([1, 2, 3, 5, 6]);
+
+tree.insert(7);
+tree.insert(4);
+tree.delete(2);
+
 tree.prettyPrint();
 
-// tree.insert(7);
-// tree.insert(4);
-// tree.delete(3);
 // console.log(tree.find(3));
 // console.log(tree.levelOrder());
 // console.log(tree.preorder());
@@ -222,5 +230,6 @@ tree.prettyPrint();
 // console.log(tree.postorder());
 // console.log("Height of Value 3: ", tree.height(tree.find(3)));
 // console.log("Height of Value 6: ", tree.height(tree.find(6)));
-console.log("Depth of Value 3: ", tree.depth(tree.find(3)));
-console.log("Depth of Value 6: ", tree.depth(tree.find(6)));
+// console.log("Depth of Value 3: ", tree.depth(tree.find(3)));
+// console.log("Depth of Value 6: ", tree.depth(tree.find(6)));
+console.log("Tree is balanced? ", tree.isBalanced());
